896e44e603cd57b361fb9459674e526e02c27a6e
FrikoGad <goliaf551@mail.ru>
2022-08-10 20:51:00 +0300

    navigation

---
+++ app/src/main/java/com/example/newsapp/ui/fragments/ArticleFragment.kt
@@ -0,0 +1,7 @@
+package com.example.newsapp.ui.fragments
+
+import androidx.fragment.app.Fragment
+import com.androiddevs.mvvmnewsapp.R
+
+class ArticleFragment : Fragment(R.layout.fragment_article) {
+}
---
+++ app/src/main/java/com/example/newsapp/ui/fragments/SearchNewsFragment.kt
@@ -0,0 +1,7 @@
+package com.example.newsapp.ui.fragments
+
+import androidx.fragment.app.Fragment
+import com.androiddevs.mvvmnewsapp.R
+
+class SearchNewsFragment : Fragment(R.layout.fragment_search_news) {
+}
