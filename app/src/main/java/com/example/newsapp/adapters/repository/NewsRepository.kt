c72cf26c54258678151a0be63d896df8ea63ac26
FrikoGad <goliaf551@mail.ru>
2022-08-14 12:37:57 +0300

    viewModels

---
+++ app/src/main/java/com/example/newsapp/adapters/repository/NewsRepository.kt
@@ -0,0 +1,8 @@
+package com.example.newsapp.adapters.repository
+
+import com.example.newsapp.db.ArticleDatabase
+
+class NewsRepository(
+    val db: ArticleDatabase
+) {
+}
--- app/src/main/java/com/example/newsapp/ui/MainActivity.kt
+++ app/src/main/java/com/example/newsapp/ui/MainActivity.kt
@@ -2,15 +2,28 @@ package com.example.newsapp.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.lifecycle.ViewModelProvider
 import androidx.navigation.fragment.findNavController
 import androidx.navigation.ui.setupWithNavController
 import com.androiddevs.mvvmnewsapp.R
+import com.example.newsapp.adapters.repository.NewsRepository
+import com.example.newsapp.db.ArticleDatabase
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var viewModel: NewsViewModel
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val  newsRepository = NewsRepository(ArticleDatabase(this))
+        val viewModelProviderFactory = NewsViewModelProviderFactory(newsRepository)
+        viewModel = ViewModelProvider(
+            this,
+            viewModelProviderFactory
+        ).get(NewsViewModel::class.java)
 
         bottom_navigation_view.setupWithNavController(news_nav_host_fragment.findNavController())
     }
